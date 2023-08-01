442068667e3f0e9ea53f739e0a0f5aff07929832
James Parkhurst <james.parkhurst@diamond.ac.uk>
2023-08-01 19:43:33 +0100

    Adding scm to setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ def main():
     Setup the package
 
     """
-    setup()
+    setup(
+        setup_requires=["setuptools_scm"],
+        use_scm_version={"write_to": "profet/_version.py"},
+    )
 
 
 if __name__ == "__main__":
