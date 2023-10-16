d638607668ce94932734a2c384235804f5edff77
James Parkhurst <james.parkhurst@diamond.ac.uk>
2023-10-16 17:12:01 +0100

    Test command line

--- _tests/test_io.py
+++ _tests/test_io.py
@@ -1,10 +1,12 @@
 import os.path
 import pytest
 import profet
+import profet.command_line
 from profet import Fetcher
 from profet import alphafold
 from profet import pdb
 from collections import defaultdict
+from contextlib import redirect_stdout
 
 ONLY_ALPHAFOLD = "F4HvG8"
 ONLY_PDB = "7U6Q"
@@ -115,3 +117,18 @@ def test_cache(tmpdir):
     assert os.path.join(tmpdir, "4v5d.cif") in items["4v5d"]
     assert os.path.join(tmpdir, "1u2p.cif") in items["1u2p"]
     assert os.path.join(tmpdir, "1u2p.pdb") in items["1u2p"]
+
+
+@pytest.mark.parametrize("test_id", get_test_ids())
+def test_command_line_main(tmpdir, test_id):
+    source, pdb_id = test_id
+
+    with open("tmp.out", "w") as outfile:
+        with redirect_stdout(outfile):
+            profet.command_line.main(
+                [pdb_id, "--save_directory", str(tmpdir), "--main_db", source]
+            )
+    with open("tmp.out") as infile:
+        lines = list(infile.readlines())
+        _, filename, _ = lines[-1].split("'")
+        assert os.path.exists(filename)
